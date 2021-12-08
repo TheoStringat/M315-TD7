7498abdbba62a7ae367f5ff34431252ef0cbdea5
Theo <theo.stringat@etu.univ-cotedazur.fr>
2021-12-08 16:31:30 +0100

    push initial

---
+++ src/TD7 - M315/Personnage.java
@@ -0,0 +1,116 @@
+package Personnage;
+
+
+/**
+* @generated
+*/
+public class Personnage {
+    
+    /**
+    * @generated
+    */
+    private String nom;
+    
+    /**
+    * @generated
+    */
+    private Integer vitalite;
+    
+    
+    /**
+    * @generated
+    */
+    private Arme armeActive;
+    
+    /**
+    * @generated
+    */
+    private Set<Arme> sacArmes;
+    
+    
+    /**
+    * @generated
+    */
+    private String getNom() {
+        return this.nom;
+    }
+    
+    /**
+    * @generated
+    */
+    private String setNom(String nom) {
+        this.nom = nom;
+    }
+    
+    /**
+    * @generated
+    */
+    private Integer getVitalite() {
+        return this.vitalite;
+    }
+    
+    /**
+    * @generated
+    */
+    private Integer setVitalite(Integer vitalite) {
+        this.vitalite = vitalite;
+    }
+    
+    
+    /**
+    * @generated
+    */
+    public Arme getSacArmes() {
+        return this.sacArmes;
+    }
+    
+    /**
+    * @generated
+    */
+    public Arme setSacArmes(Arme sacArmes) {
+        this.sacArmes = sacArmes;
+    }
+    
+    /**
+    * @generated
+    */
+    public Arme getArmeActive() {
+        return this.armeActive;
+    }
+    
+    /**
+    * @generated
+    */
+    public Arme setArmeActive(Arme armeActive) {
+        this.armeActive = armeActive;
+    }
+    
+
+    //                          Operations                                  
+    
+    /**
+    * @generated
+    */
+    public changeArme() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public getForce() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public getProtection() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public attaquer() {
+        //TODO
+    }
+    
+}
